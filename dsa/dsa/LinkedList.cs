bae6de5cd3ea749d96689822df43b488a9dc3192
pcfinfyuser@gmail.com <pcfinfyuser@gmail.com>
2019-12-23 10:08:43 -0800

    Push to My Repo

---
+++ dsa/dsa/LinkedList.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dsa
+{
+    public class LinkedListNode
+    {
+        public int Value { get; set; }
+
+        public LinkedListNode(int value)
+        {
+            Value = value;
+        }
+
+        public LinkedListNode Next { get; set; }
+    }
+    class LinkedList 
+    {
+        //LinkedListNode node = new LinkedListNode(3);
+
+        public LinkedListNode Head { get; private set; }
+        public LinkedListNode Tail { get; private set; }
+
+        int count = 0;
+
+        public void AddFirst(int value)
+        {
+            AddFirst(new LinkedListNode(value));
+        }
+
+        public void AddFirst(LinkedListNode node)
+        {
+            LinkedListNode temp = Head;
+            if(count == 0)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                temp = Head;
+                Head = node;
+                Head.Next = temp;
+            }
+            count++;
+        }
+
+        public void AddLast(int value)
+        {
+            AddLast(new LinkedListNode(value));
+        }
+
+        public void AddLast(LinkedListNode node)
+        {
+            if(count == 0)
+            {
+                Head = node;
+            }
+            else
+            {
+                Tail.Next = node;
+            }
+
+            Tail = node;
+            count++;
+        }
+
+        public void RemoveFirst()
+        {
+            if (count == 0)
+            {
+                return;
+            }
+            else if(count == 1)
+            {
+                Head = null;
+                Tail = null;
+            }
+            else
+            {
+                Head = Head.Next;
+            }
+
+            count--;
+        }
+
+        public void RemoveLast()
+        {
+            if(count == 0)
+            {
+                return;
+            }
+            else if(count == 1)
+            {
+                Head = null;
+                Tail = null;
+            }
+            else
+            {
+                LinkedListNode current = Head;
+                while(current.Next != Tail)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = null;
+                Tail = current;
+            }
+        }
+
+        public void Remove(int value)
+        {
+            LinkedListNode previous = Head;
+            LinkedListNode current = Head;
+            if(count == 1 && Head.Value == value)
+            {
+                Head = null;
+                Tail = null;
+            }
+            else if(count > 1)
+            {
+                while(current.Value != value)
+                {
+                    previous = current;
+                    current = current.Next;
+                }
+
+                previous.Next = current.Next;
+            }
+
+            count--;
+        }
+
+        public IEnumerable<int> GetEnumrator()
+        {
+            LinkedListNode current = Head;
+            while(current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ dsa/dsa/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dsa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList llst = new LinkedList();
+
+
+            //PrintLinkedList(llst);
+            //llst.AddFirst(3);
+            //llst.AddFirst(5);
+            //llst.AddFirst(7);
+            llst.AddLast(9);
+            llst.AddLast(11);
+            llst.AddLast(13);
+            llst.AddLast(15);
+            llst.AddLast(17);
+            llst.AddLast(19);
+            llst.AddLast(21);
+            llst.AddLast(23);
+
+            PrintLinkedList(llst);
+            Console.WriteLine("Remove 1");
+            llst.Remove(17);
+            PrintLinkedList(llst);
+
+            Console.WriteLine("Remove 2");
+            llst.Remove(13);
+            PrintLinkedList(llst);
+
+            Console.WriteLine("Remove 3");
+            llst.Remove(21);
+            PrintLinkedList(llst);
+
+            Console.WriteLine("Remove 4");
+            llst.Remove(23);
+            PrintLinkedList(llst);
+            Console.Read();
+
+
+            Console.WriteLine("Remove 5");
+            llst.Remove(9);
+            PrintLinkedList(llst);
+            Console.Read();
+            void PrintLinkedList(LinkedList locallst)
+            {
+                foreach (int val in locallst.GetEnumrator())
+                {
+                    Console.WriteLine(val);
+                }
+            }
+
+        }
+    }
+}
